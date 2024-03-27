e7dd0da4837fb4d57b0c712dd36ad5cfc535dc8e
brunolucy2023x <lucyb5147@gmail.com>
2024-03-27 04:45:38 -0700

    101

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,6 +5,7 @@
  * @first: First node
  * @second: Second node
  * Return: the node of the ancestor
+ * Return: the node of the ancestor
  */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
